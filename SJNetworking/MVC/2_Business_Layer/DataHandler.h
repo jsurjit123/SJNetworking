1054f2824b7e2dd4ec350c8b18d6e761e6e49ff0
Surjit Joshi <surjitjoshi@pu2l1100.local>
2017-12-28 17:15:17 +0530

    Added Business Layer files

---
+++ SJNetworking/MVC/2_Business_Layer/DataHandler.h
@@ -0,0 +1,13 @@
+//
+//  DataHandler.h
+//  SJNetworking
+//
+//  Created by Surjit Joshi on 28/12/17.
+//  Copyright © 2017 Surjit Joshi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataHandler : NSObject
+
+@end
---
+++ SJNetworking/MVC/2_Business_Layer/DataHandler.m
@@ -0,0 +1,13 @@
+//
+//  DataHandler.m
+//  SJNetworking
+//
+//  Created by Surjit Joshi on 28/12/17.
+//  Copyright © 2017 Surjit Joshi. All rights reserved.
+//
+
+#import "DataHandler.h"
+
+@implementation DataHandler
+
+@end
