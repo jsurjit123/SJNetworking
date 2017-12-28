7a179d564ace1985a55ecbfb34df72fe8a36e7bc
Surjit Joshi <surjitjoshi@pu2l1100.local>
2017-12-28 17:16:11 +0530

    Added Data Layer files

---
+++ SJNetworking/MVC/3_Data_Layer/DataProvider.h
@@ -0,0 +1,13 @@
+//
+//  DataProvider.h
+//  SJNetworking
+//
+//  Created by Surjit Joshi on 28/12/17.
+//  Copyright © 2017 Surjit Joshi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DataProvider : NSObject
+
+@end
---
+++ SJNetworking/MVC/3_Data_Layer/DataProvider.m
@@ -0,0 +1,13 @@
+//
+//  DataProvider.m
+//  SJNetworking
+//
+//  Created by Surjit Joshi on 28/12/17.
+//  Copyright © 2017 Surjit Joshi. All rights reserved.
+//
+
+#import "DataProvider.h"
+
+@implementation DataProvider
+
+@end
