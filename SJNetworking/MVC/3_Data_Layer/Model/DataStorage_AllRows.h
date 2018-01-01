add07fd4b51ba1ee676a36b6efc2f4e0ab6c2584
Surjit Joshi <surjitjoshi@pu2l1100.local>
2018-01-01 17:08:02 +0530

    Code comment and restructuring

--- SJNetworking/MVC/3_Data_Layer/Model/DataStorage_AllRows.h
+++ SJNetworking/MVC/3_Data_Layer/Model/DataStorage_AllRows.h
@@ -6,20 +6,46 @@
 //  Copyright © 2017 Surjit Joshi. All rights reserved.
 //
 
+/******************************************************************************
+ *                                     Header Files
+ ******************************************************************************/
+
 #import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+/******************************************************************************
+ *                                     Interface Declaration
+ ******************************************************************************/
 
 @interface DataStorage_AllRows : NSObject
+
+
+/******************************************************************************
+ *           Public Variables
+ ******************************************************************************/
 
 @property (nonatomic, retain)  NSString *mainTitle;
 @property (nonatomic, retain)  NSMutableArray *allRowsArray;
 
 @end
 
+/******************************************************************************
+ *                                     Interface Declaration
+ ******************************************************************************/
+
+
+
 
 @interface DataStorage_OneRow : NSObject
+
+/******************************************************************************
+ *           Public Variables
+ ******************************************************************************/
 
 @property (nonatomic, retain)  NSString *title;
 @property (nonatomic, retain)  NSString *description;
 @property (nonatomic, retain)  NSString *imageHref;
+@property (nonatomic, retain)  UIImage  *iconImage;
+@property                      CGFloat   rowHeight;
 
 @end
