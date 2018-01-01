1501ee3ac570563e8c0aab8cc94cdf35df434fd0
Surjit Joshi <surjitjoshi@pu2l1100.local>
2018-01-01 17:18:19 +0530

    - Fixed bug for Latin string encoding for JSON - Code comments and re-structuring

--- SJNetworking/MVC/2_Business_Layer/DataHandler.m
+++ SJNetworking/MVC/2_Business_Layer/DataHandler.m
@@ -6,8 +6,16 @@
 //  Copyright © 2017 Surjit Joshi. All rights reserved.
 //
 
+/******************************************************************************
+ *                                     Header Files
+ ******************************************************************************/
+
 #import "DataHandler.h"
 #import "AppDelegate.h"
+
+/******************************************************************************
+ *                                     Interface Declation
+ ******************************************************************************/
 
 @interface DataHandler()
 {
@@ -16,8 +24,28 @@
 
 @end
 
+/******************************************************************************
+ *                                     Interface Definition
+ ******************************************************************************/
+
 @implementation DataHandler
 @synthesize dataProvider;
+
+/*
+ * Function Name        : init
+ *
+ * Author               : Surjit Joshi
+ *
+ * Description          : Start the Data Layer and receives JSON data
+ *
+ * Detailed description : -
+ *
+ * ParaIn               : -
+ *
+ * ParaOut              : -
+ *
+ * Return               : -
+ */
 
 // this method called when object of this class is instantiated
 -(id)init
@@ -26,66 +54,124 @@
     
     if(self)
     {
+        // object of AppDelegate to access objects and methods defined in AppDelegate implementation
         mainDelegate = (AppDelegate *) [[UIApplication sharedApplication] delegate];
 
+        // add listener for "JSONData" message to be broadcasted by Data layer class
         [[NSNotificationCenter defaultCenter] addObserver:self
                                                  selector:@selector(GET_JSONData:)
                                                      name:@"JSONData" object:nil];
         
+        // initialize Data Layer
         self.dataProvider = [[DataProvider alloc] initWithURLStr:@"https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"];
     }
     
     return self;
 }
 
+
+/*
+ * Function Name        : stopDataDownload
+ *
+ * Author               : Surjit Joshi
+ *
+ * Description          : Destroys Business Layer classes
+ *
+ * Detailed description : -
+ *
+ * ParaIn               : -
+ *
+ * ParaOut              : -
+ *
+ * Return               : -
+ */
+
 -(void) stopSendingData
 {
+    // stop listening to message
     [[NSNotificationCenter defaultCenter] removeObserver:self name:@"JSONData" object:nil];
+    
+    // destroy objects
     [self.dataProvider stopUpdating];
     self.dataProvider = nil;
 }
+
+
+/*
+ * Function Name        : GET_JSONData
+ *
+ * Author               : Surjit Joshi
+ *
+ * Description          : Receives JSON data from Data Layer, parses it and send to UI Layer
+ *
+ * Detailed description : -
+ *
+ * ParaIn               : pObjNotification - provides row JSON data
+ *
+ * ParaOut              : -
+ *
+ * Return               : -
+ */
 
 // update screen data
 - (void) GET_JSONData:(NSNotification *)pObjNotification
 {
     if(pObjNotification == nil)
     {
-        return;
+        // broadcast the data
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"Display_RowData" object:@"DataNotReceived"];
     }
     
     if(pObjNotification.object == nil)
     {
-        return;
+        // broadcast the data
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"Display_RowData" object:@"DataNotReceived"];
     }
     
     if([pObjNotification.object isKindOfClass:[NSMutableData class]])
     {
+        // convert row data to NSMutableData
         NSMutableData *data = (NSMutableData *) pObjNotification.object;
+        
+        // convert raw data to string using "NSISOLatin1StringEncoding" encoding
+        // NSISOLatin1StringEncoding is required as there are special characters in JSON information
+        
         NSString *dataFromJSON = [[NSString alloc] initWithData:data encoding:NSISOLatin1StringEncoding];
         NSLog(@"Before parsing: %@", dataFromJSON);
 
+        // convert String to Data using "NSUTF8StringEncoding" encoding
+        // this extra step is required to overcome issue of difference between "NSISOLatin1StringEncoding" and "NSUTF8StringEncoding"
         NSData *resData = [dataFromJSON dataUsingEncoding:NSUTF8StringEncoding];
 
+        // use default parser to serialize JSON data
         NSError *localError = nil;
         NSDictionary *parsedObject = [NSJSONSerialization JSONObjectWithData:resData options:NSJSONReadingAllowFragments error:&localError];
         
+        // check for parser error
         if (localError != nil)
         {
             NSLog(@"Parser Error");
+            
+            // broadcast the data
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"Display_RowData" object:@"DataNotReceived"];
         }
         else
         {
+            // store title
             mainDelegate.dataStorage_AllRows.mainTitle = [parsedObject valueForKey:@"title"];
+            
+            // store all rows to array
             mainDelegate.dataStorage_AllRows.allRowsArray = [[NSMutableArray alloc] init];
             
             NSArray *allDataRowsOfJSON = [parsedObject valueForKey:@"rows"];
-            NSLog(@"Count %lu", (unsigned long)allDataRowsOfJSON.count);
+            //NSLog(@"Count %lu", (unsigned long)allDataRowsOfJSON.count);
             
             for (NSDictionary *oneRowDict in allDataRowsOfJSON)
             {
+                // create one row information
                 DataStorage_OneRow *dataStorage_OneRow = [[DataStorage_OneRow alloc] init];
                 
-                
+                // check for NULL
                 if(!([oneRowDict objectForKey:@"title"] == nil ||
                      [[oneRowDict objectForKey:@"title"] isKindOfClass:[NSNull class]] ||
                      [[oneRowDict objectForKey:@"title"] length] == 0))
@@ -94,24 +180,25 @@
                 }
                 else
                 {
+                    // default value
                     dataStorage_OneRow.title = @" ";
                 }
                 
-                
+                // check for NULL
                 if(!([oneRowDict objectForKey:@"description"] == nil ||
                      [[oneRowDict objectForKey:@"description"] isKindOfClass:[NSNull class]] ||
                      [[oneRowDict objectForKey:@"description"] length] == 0))
                 {
                     dataStorage_OneRow.description = [oneRowDict objectForKey:@"description"];
-                    
-                    //dataStorage_OneRow.description = @"Warmer than you might think.";
                 }
                 else
                 {
+                    // default value
                     dataStorage_OneRow.description = @" ";
                 }
 
                 
+                // check for NULL
                 if(!([oneRowDict objectForKey:@"imageHref"] == nil ||
                      [[oneRowDict objectForKey:@"imageHref"] isKindOfClass:[NSNull class]] ||
                      [[oneRowDict objectForKey:@"imageHref"] length] == 0))
@@ -120,6 +207,7 @@
                 }
                 else
                 {
+                    // default value
                     dataStorage_OneRow.imageHref = @" ";
                 }
                 
@@ -128,18 +216,19 @@
                    [dataStorage_OneRow.description isEqualToString:@" "] &&
                    [dataStorage_OneRow.imageHref isEqualToString:@" "]))
                 {
+                    // add one row information if all the objects are valid
                     [mainDelegate.dataStorage_AllRows.allRowsArray addObject:dataStorage_OneRow];
                 }
             }
         }
         
-        NSLog(@"Title : %@", mainDelegate.dataStorage_AllRows.mainTitle);
-        NSLog(@"allRowsArray : %@", mainDelegate.dataStorage_AllRows.allRowsArray);
-
-        
         // broadcast the data
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"Display_RowData" object:nil];
-        
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"Display_RowData" object:@"DataReceived"];
+    }
+    else
+    {
+        // broadcast the data
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"Display_RowData" object:@"DataNotReceived"];
     }
 }
 
